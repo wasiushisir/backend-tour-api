7fa02c5e04cbf249bfad273871a9b66a00ad9e8b
wasiushisir <wasiuahmed410@gmail.com>
2022-09-21 20:16:49 +0600

    deploy

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(express.json())
 app.use(cors())
 
 
-app.use('https://backend-tour-api.vercel.app/api',tourRoute)
+app.use('/api',tourRoute)
 
 
-app.use('https://backend-tour-api.vercel.app/', (req, res) => {
+app.use('/', (req, res) => {
     res.send('route is working!')
   })
 
