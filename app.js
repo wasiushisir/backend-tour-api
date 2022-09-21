2373b97b370f816615025c17234f7f766e62819c
wasiushisir <wasiuahmed410@gmail.com>
2022-09-21 21:42:29 +0600

    h

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(express.json())
 app.use(cors())
 
 
-app.use('/api',tourRoute)
+app.use('https://backend-tour-7zr650m2w-wasiushisir.vercel.app/api',tourRoute)
 
 
-app.get('/', (req, res) => {
+app.get('https://backend-tour-7zr650m2w-wasiushisir.vercel.app/', (req, res) => {
     res.send('route is working!')
   })
 
